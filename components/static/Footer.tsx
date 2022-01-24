e26539442fa98d33ca801bdb582d293c4591a60f
Alex Ionitel <oranggsoda@gmail.com>
2022-01-23 19:23:52 -0500

    added social media icons

--- components/static/Footer.tsx
+++ components/static/Footer.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import NavButton from '../utils/NavButton'
+import fb from '../../assets/fb.png'
+import Image from 'next/image'
+import tiktok from '../../assets/tiktok.png'
+
+const socialLinksSize = 30;
 
 const Footer: React.FC = () => {
   return (
     <div className='border-t border-gray my-6 font-karla'>
-      <ul className='md:flex mt-2 justify-center'>
+      <ul className='md:flex justify-center my-10'>
         <FooterButton path='/about' name='About' />
         <FooterButton path='/contact' name='Contact' />
         <FooterButton path='/refunds' name='Refunds and Cancellations' />
         <FooterButton path='/privacy' name='Privacy Policy' />
         <FooterButton path='/faq' name='FAQ' />
       </ul>
+      <div className='flex md:justify-end justify-center'>
+        <Image src={fb} height={socialLinksSize} width={socialLinksSize} />
+        <Image src={tiktok} height={socialLinksSize} width={socialLinksSize} />
+      </div>
     </div>
   )
 }
