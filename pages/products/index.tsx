03582b6a956c72305d9ae3cde3da8e1e27617afc
Alex Ionitel <oranggsoda@gmail.com>
2022-02-02 13:34:29 -0500

    fixed response data parsing

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -2,30 +2,25 @@ import { NextPage } from 'next'
 import React from 'react'
 import axios from 'axios'
 import Image from 'next/image'
-import Head from 'next/head'
 
 const url = 'http://localhost:4000/api/products'
 
-const Product: NextPage = ({ product }: any) => {
+const Product: NextPage = ({ products }: any) => {
   return (
-    <>
-      <Head>
-        <title>Shop - Dippped</title>
-      </Head>
-      <div>
-        <h1>{product.data[0].name}</h1>
-        <img src={product.data[0].image[0] as string} width={200} height={200} />
-      </div>
-    </>
+    <div>
+      <h1>{products[0].name}</h1>
+      <Image src={products[0].image[0]} height={100} width={100} />
+    </div>
   )
 }
 
 export const getStaticProps = async () => {
-  const {data} = await axios.get(url)
+  const { data } = await axios.get(url)
+  const products = data.data
   
   return {
     props: {
-      product: data
+      products
     }
   }
 }
