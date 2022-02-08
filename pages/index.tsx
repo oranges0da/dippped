3eeb3d165dcdf89c2d0e06ba447505d798b8ad01
Alex Ionitel <oranggsoda@gmail.com>
2022-02-08 01:02:49 -0500

    better bestseller card logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import NavButton from '../components/utils/NavButton'
 import axios from 'axios'
+import Link from 'next/link'
+
+const BestSellerData = [
+  {
+    title: "Valentine's Day Box",
+    price: 40,
+    image: 'https://i.imgur.com/R1YVxMF.jpg'
+  }, {
+    title: "Mini Wedding Box",
+    price: 20,
+    image: 'https://i.imgur.com/yYptDSk.jpg'
+  }
+]
 
 const Home: NextPage = ({ props }) => {
   return (
@@ -27,6 +40,11 @@ const Home: NextPage = ({ props }) => {
       </div>
     </div>
     <h1 className='md:ml-10 md:mt-10 ml-4 mt-6 font-quando text-2xl'>Bestsellers</h1>
+    {BestSellerData.map((item, index) => (
+      <div key={index}>
+        <BestsellerCard title={item.title} price={item.price} image={item.image} />
+      </div>
+    ))}
     </div>
     </>
   )
@@ -41,12 +59,22 @@ const ShopNowButton: React.FC = () => {
   )
 }
 
-// bestsellers
-const BestsellerCard: React.FC = () => {
-  return (
-    <div>
+interface BestsellerCardProps {
+  title: string,
+  price: number,
+  image: string
+}
 
-    </div>
+// bestsellers
+const BestsellerCard: React.FC<BestsellerCardProps> = ({ title, price, image }) => {
+  return (
+    <Link href='/products'>
+      <div className='hover:cursor-pointer'>
+        <Image src={image} width={100} height={100} />
+        <h1>{title}</h1>
+        <h1>{price}</h1>
+      </div>
+    </Link>
   )
 }
 
