5dfa96b5ff13b378be758499caff7c98c3914c1c
Alex Ionitel <oranggsoda@gmail.com>
2022-02-11 11:40:57 -0500

    better contact form style and logic

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 
 const ContactPage: NextPage = () => {
   return (
-    <div className='align-center'>
+    <div className='mx-20 my-10 font-karla'>
+      <h1 className='text-center mb-10'>Contact Us</h1>
       <div className="w-full max-w-lg">
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
               First Name
             </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" />
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder='John' />
         </div>
         <div className="w-full md:w-1/2 px-3">
           <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
@@ -25,7 +26,7 @@ const ContactPage: NextPage = () => {
           <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
             E-mail
           </label>
-          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" />
+          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="example@gmail.com" />
         </div>
       </div>
       <div className="flex flex-wrap -mx-3 mb-6">
@@ -33,12 +34,12 @@ const ContactPage: NextPage = () => {
           <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
             Message
           </label>
-          <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
+          <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
         </div>
       </div>
       <div className="md:flex md:items-center">
         <div className="md:w-1/3">
-          <button className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-black font-bold py-2 px-4 rounded" type="button">
+          <button className="align-center border border-gray-200 bg-teal-400 hover:bg-teal-400 focus:outline-none text-black font-bold py-2 px-4 rounded" type="button">
             Submit
           </button>
         </div>
