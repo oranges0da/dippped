da445cc715e74f0b980a21d4eb9141d92dd9e88d
Alex Ionitel <oranggsoda@gmail.com>
2022-02-15 18:56:30 -0500

    quantity logic finished

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import Head from 'next/head'
 import { useRecoilState } from 'recoil'
@@ -7,6 +7,19 @@ import Image from 'next/image'
 
 const Product = ({ product }) => {
   const [cartItem, setCartItem] = useRecoilState(cartAtom)
+  const [quantity, setQuantity] = useState(1)
+
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity => quantity -= 1)
+    }
+  }
+
+  const increaseQuantity = () => {
+    if (quantity < 10) {
+      setQuantity(quantity => quantity += 1)
+    }
+  }
 
   const handleAddCart = () => {
     setCartItem(oldCartItems => [
@@ -15,7 +28,7 @@ const Product = ({ product }) => {
         name: product.name,
         price: product.price,
         size: 'large',
-        quantity: 1,
+        quantity,
         images: product.images
       }
     ])
@@ -36,26 +49,23 @@ const Product = ({ product }) => {
                   ))}
                 </div>
                 <div className="w-full max-w-lg mx-auto mt-5 md:ml-8 md:mt-0 md:w-1/2">
-                    <h3 className="text-gray-700 text-lg">{product.name}</h3>
-                    <span className="text-gray-500 mt-3">${product.price}.00</span>
+                    <h1>{product.name}</h1>
+                    <h2 className='text-sm my-2'>{product.desc}</h2>
+                    <span className="mt-3">${product.price}.00</span>
                     <div className="mt-2">
                         <div className="flex items-center mt-1">
-                            <button className="text-gray-500 focus:outline-none focus:text-gray-600">
+                            <button onClick={() => {decreaseQuantity()}}>
                               <svg className="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                             </button>
-                            <span className="text-gray-700 text-lg mx-2">{product.quantity}</span>
-                            <button className="text-gray-500 focus:outline-none focus:text-gray-600">
+                            <h1 className='mx-2'>{quantity}</h1>
+                            <button onClick={() => {increaseQuantity()}}>
                                 <svg className="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                             </button>
                         </div>
                     </div>
-                    <button className=''>
-                        <button className='hover:cursor-pointer mt-5 bg-gray'>
-                          <div className='bg-gray text-center'>
-                            <h1>Add to Cart</h1>
-                          </div>
-                        </button>
-                    </button>
+                    <div className='text-center'>
+                      <button className='hover:cursor-pointer'>Add to Cart</button>
+                    </div>
                 </div>
             </div>
           </div>
