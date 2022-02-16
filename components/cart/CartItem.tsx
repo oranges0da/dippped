d700c59215b12c86ba57476c8607900b88f49881
Alex Ionitel <oranggsoda@gmail.com>
2022-02-16 12:56:25 -0500

    basic delete item from cart working

--- components/cart/CartItem.tsx
+++ components/cart/CartItem.tsx
@@ -12,14 +12,19 @@ const CartItem: React.FC<CartItemProps> = ({ id, name, price }) => {
   const [cartItems, setCartItems] = useRecoilState(cartAtom)
 
   const handleDelete = () => {
+    setCartItems(oldCartItems => oldCartItems.filter(item => item.id !== id))
 
+    console.log("Item deleted.")
   }
 
   return (
     <div>
       <h1>{name}</h1>
       <h1>{price}</h1>
-      <button className='border border-gray my-5'>Delete</button>
+      <button 
+        className='border border-gray my-5'
+        onClick={() => handleDelete()}
+      >Delete</button>
     </div>
   )
 }
