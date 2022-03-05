7e5d54ea19fbdad592c939515a689d13a3ecb1ba
Alex Ionitel <oranggsoda@gmail.com>
2022-03-05 12:20:24 -0500

    cart logic working a bit

--- components/nav/Footer.tsx
+++ components/nav/Footer.tsx
@@ -6,8 +6,8 @@ const Footer: React.FC = () => {
     <div className='border-t border-gray my-6 font-karla text-sm'>
       <div className='flex md:justify-end justify-center my-14 md:mr-8'>
         <FooterButton source='https://i.imgur.com/fo9OVTB.png' size={38} path='https://www.facebook.com' />
-        <FooterButton source='https://i.imgur.com/buM9fEc.png' size={35} path='https://www.tiktok.com' />
-        <FooterButton source='https://i.imgur.com/E5t7hMe.png' size={36} path='https://www.instagram.com' />
+        <FooterButton source='https://i.imgur.com/buM9fEc.png' size={35} path='https://www.tiktok.com/@dipppedberries?lang=en' />
+        <FooterButton source='https://i.imgur.com/E5t7hMe.png' size={36} path='https://www.instagram.com/dipppedberries/' />
       </div>
     </div>
   )
--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -5,22 +5,26 @@ import cartAtom from '../../state/atoms'
 import CartItem from '../../components/cart/CartItem'
 import Head from 'next/head'
 
-
 const Cart: NextPage = () => {
   const cartItems = useRecoilValue(cartAtom)
 
-  return (
-    <>
-      <Head>
-        <title>Cart - Dippped</title>
-      </Head>
+  console.log(cartItems)
+  
+  if (cartItems.length === 0) {
+    return (
+      <div>
+        <h1>Nothing is in cart.</h1>
+      </div>
+    )
+  } else {
+    return (
       <div>
         {cartItems.map(item => (
           <CartItem id={item.id} name={item.name} price={item.price} image={item.images[0]} />
         ))}
       </div>
-    </>
-  )
+    )
+  }
 }
 
 export default Cart
