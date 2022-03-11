43206f5f31ba02417e1005ee658bbfb24857b328
Alex Ionitel <oranggsoda@gmail.com>
2022-03-11 12:32:53 -0500

    stripe working with pre-specified products, now have to make it work for items in cart

--- components/cart/HasCartItems.tsx
+++ components/cart/HasCartItems.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState} from 'react'
 import CartItem from './CartItem'
 import { useRecoilValue } from 'recoil'
 import cartAtom from '../../state/atoms'
+import { loadStripe } from '@stripe/stripe-js'
+
+const stripePromise = loadStripe('pk_test_51KOR9pKdGoM4dCrlq6pFdVhbnbN71JOKhx8VkxWtiDU8LU2s3FjWHZLI8Zn8HwXbmtZtAYUweAQVjlzp3vJJptvJ00D2Gl3i4B')
 
 const HasCartItems: React.FC = () => {
   const cartItems = useRecoilValue(cartAtom)
+  const [stripeErr, setStripeErr] = useState<string | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const handleCheckout = async () => {
+    setLoading(true)
+
+    const stripe = await stripePromise
+
+    const checkout = await stripe?.redirectToCheckout({
+      lineItems: [{
+        price: 'price_1KbvmtKdGoM4dCrl56DfkxfC',
+        quantity: 1
+      }, {
+        price: 'price_1KbwViKdGoM4dCrlcxyt3oCK',
+        quantity: 1
+      }],
+      mode: "payment",
+      cancelUrl: 'http://localhost:3000/cart',
+      successUrl: 'http://localhost:3000/success'
+    })
+
+    setLoading(false)
+  }
 
   return (
     <div className='flex-row justify-start'>
@@ -13,7 +39,9 @@ const HasCartItems: React.FC = () => {
           <CartItem id={item.id} name={item.name} price={item.price} image={item.images[0]} />
         ))}
       </div>
-      <h1 className='text-center md:text-right'>Checkout</h1>
+      <div onClick={() => handleCheckout()} className='bg-black hover:cursor-pointer'>
+        <h1 className='text-white'>Checkout</h1>
+      </div>
     </div>
   )
 }
---
+++ pages/success/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+const Success: NextPage = () => {
+  return (
+    <div>
+      Thank you for ordering
+    </div>
+  )
+}
+
+export default Success
