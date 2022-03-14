e8df454fd7e65beeeb31182033aa5b3254028d05
Alex Ionitel <oranggsoda@gmail.com>
2022-03-13 21:51:09 -0400

    added react-datepicker

--- components/cart/Schedule.tsx
+++ components/cart/Schedule.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import Calendar from 'react-calendar'
+import { useState } from 'react';
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const Schedule: React.FC = () => {
+  const [startDate, setStartDate] = useState<any>(new Date())
+
   return (
     <div>
-      <Calendar />
+      <DatePicker selected={startDate} onChange={(date:Date) => setStartDate(date)} />
     </div>
   )
 }
