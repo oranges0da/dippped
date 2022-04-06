020aa95eb402e8f013ab5fbf1d96d6c6c75d4d2e
Alex Ionitel <oranggsoda@gmail.com>
2022-04-06 15:36:12 -0400

    keeping track of contact form inputs

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,8 +1,13 @@
 import { NextPage } from 'next'
-import React from 'react'
+import React, { useState, useEffect, } from 'react'
 import Head from 'next/head'
 
 const ContactPage: NextPage = () => {
+  const [firstName, setName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
   return (
     <>
       <Head>
@@ -16,13 +21,13 @@ const ContactPage: NextPage = () => {
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                 First Name
               </label>
-              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder='John' />
+              <input onChange={e => setName(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder='John' />
           </div>
           <div className="w-full md:w-1/2 px-3">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
               Last Name
             </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
+            <input onChange={e => setLastName(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
           </div>
           </div>
         </div>
@@ -31,7 +36,7 @@ const ContactPage: NextPage = () => {
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
               E-mail
             </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="example@gmail.com" />
+            <input onChange={e => setEmail(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="example@gmail.com" />
           </div>
         </div>
         <div className="flex flex-wrap -mx-3 mb-6">
@@ -39,7 +44,7 @@ const ContactPage: NextPage = () => {
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
               Message
             </label>
-            <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" />
+            <textarea onChange={e => setMessage(e.target.value)} className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" />
           </div>
         </div>
         <div className="md:flex md:items-center">
@@ -49,6 +54,12 @@ const ContactPage: NextPage = () => {
             </button>
           </div>
           <div className="md:w-2/3"></div>
+        </div>
+        <div>
+          First Name: {firstName} <br />
+          Last Name: {lastName} <br />
+          Email: {email} <br />
+          Message: {message} <br />
         </div>
       </div>
     </>
