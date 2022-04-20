0a0526de666304850a635b7ac8d582910e962b5b
Alex Ionitel <oranggsoda@gmail.com>
2022-04-20 19:17:40 -0400

    different social media icons

--- components/nav/Footer.tsx
+++ components/nav/Footer.tsx
@@ -6,9 +6,9 @@ const Footer: React.FC = () => {
   return (
     <div className='border-t border-gray my-6 font-karla text-sm'>
       <div className='flex md:justify-end justify-center my-14 md:mr-8'>
-        <FooterButton source='https://i.imgur.com/fo9OVTB.png' size={38} path='https://www.facebook.com' />
-        <FooterButton source='https://i.imgur.com/buM9fEc.png' size={35} path='https://www.tiktok.com/@dipppedberries?lang=en' />
-        <FooterButton source='https://i.imgur.com/E5t7hMe.png' size={36} path='https://www.instagram.com/dipppedberries/' />
+        <FooterButton source='https://i.imgur.com/fo9OVTB.png' size={28} path='https://www.facebook.com' />
+        <FooterButton source='https://i.imgur.com/buM9fEc.png' size={25} path='https://www.tiktok.com/@dipppedberries?lang=en' />
+        <FooterButton source='https://i.imgur.com/E5t7hMe.png' size={26} path='https://www.instagram.com/dipppedberries/' />
       </div>
     </div>
   )
@@ -22,7 +22,7 @@ interface FooterButtonProps {
 
 const FooterButton: React.FC<FooterButtonProps> = ({ source, path, size }) => {
   return (
-    <a href={path} className='mx-2 sm:hover:scale-110 sm:transition-all' rel="noopener noreferrer" target='_blank'>
+    <a href={path} className='mx-3 sm:hover:scale-110 sm:transition-all' rel="noopener noreferrer" target='_blank'>
       <Image src={source} height={size} width={size} />
     </a>
   )
