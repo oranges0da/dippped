299787aebf316aa5cd910e72d9ba8df33fdc2e04
Alex Ionitel <oranggsoda@gmail.com>
2022-04-27 23:13:09 -0400

    added framer-motion and ProductCard fading in on load

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { CgArrowLongRight } from 'react-icons/cg'
+import { motion } from 'framer-motion'
 
 interface ProductCardProps {
   id: number,
@@ -14,14 +15,18 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({ id, title, price, image, showPrice, showArrow }) => {
   return (
     <Link href={`products/${id}`}>
-      <div className='hover:cursor-pointer mt-8 bg-pink font-karla font-semibold rounded mx-4'>
+      <motion.div 
+        className='hover:cursor-pointer mt-8 bg-pink font-karla font-semibold rounded mx-4'
+        initial={{ opacity:0 }}
+        animate={{ opacity:1 }}
+      >
         <img src={image} />
         <div className='flex justify-between mx-3 px-2 pt-4 pb-4'>
           <h1 className='font-quando text-sm mt-1'>{title}</h1>
           {showPrice && <h1>${price}.00</h1>}
           {showArrow && <CgArrowLongRight className='mt-1 mr-1 sm:hover:scale-125 transition-all' />}
         </div>
-      </div>
+      </motion.div>
     </Link>
   )
 }
